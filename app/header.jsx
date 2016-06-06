704c664850aaf2ebdd19609531a1c66b0af625ea
johel <johel.carvalho@gmail.com>
2016-06-06 09:22:58 -0300

    leve modificação de estilo e retificada mistura de espaços e tabs apontada pelo lint

--- app/header.jsx
+++ app/header.jsx
@@ -4,12 +4,12 @@ export default class header extends Component{
 	render(){
 		return(
 			<div className="page-header">
-		  	<div className = "page-logo">
-		  		<img src="http://www.keenthemes.com/preview/metronic/theme/assets/layouts/layout/img/logo.png"
-		  	 			alt="logo" className="logo-default" />
-		  	</div>
-		  	<button className="btn btn-default"> Hey you</button>
-		  </div>
+				<div className = "page-logo">
+					<img src="http://www.keenthemes.com/preview/metronic/theme/assets/layouts/layout/img/logo.png"
+						alt="logo" className="logo-default" />
+				</div>
+				<button className="btn btn-default"> Hey you</button>
+			</div>
 		)
 	}
 }
