3c3a8a7457e1521ea667aead62362de23c68ba51
johel <johel.carvalho@gmail.com>
2016-06-06 19:07:46 -0300

    criação de um navbar simples, utilizando somente o componente Navdropdown do react-bootstrap

--- app/header.jsx
+++ app/header.jsx
@@ -1,14 +1,35 @@
 import React, {Component} from 'react';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem, ProgressBar} from 'react-bootstrap';
 
 export default class header extends Component{
 	render(){
 		return(
 			<div className="page-header">
 				<div className = "page-logo">
-					<img src="http://www.keenthemes.com/preview/metronic/theme/assets/layouts/layout/img/logo.png"
-						alt="logo" className="logo-default" />
+					<img className="nav-logo" src="http://www.keenthemes.com/preview/metronic/theme/assets/layouts/layout/img/logo.png"
+						alt="logo"/>
 				</div>
-				<button className="btn btn-default"> Hey you</button>
+
+
+					<ul className="nav navbar-top-links navbar-right">	
+						<NavDropdown title={<div><i className="glyphicon glyphicon-user"></i> &nbsp;</div>} >
+							<MenuItem eventKey="1">
+								<a href="">
+									<i className="glyphicon glyphicon-user"></i>{" "} User Profile
+								</a>
+							</MenuItem> 
+							<MenuItem eventKey="2">
+								<a href="">
+									<i className="glyphicon glyphicon-cog"></i>{" "} Settings
+								</a>
+							</MenuItem> 
+							<MenuItem eventKey="3">
+								<a href="">
+									<i className="glyphicon glyphicon-off"></i>{" "} Logout
+								</a>
+							</MenuItem>
+						</NavDropdown>
+				</ul> 
 			</div>
 		)
 	}
